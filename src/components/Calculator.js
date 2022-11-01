6fabbaf3e4e1f53c464e956d0046d5bbd7f65454
Matyas Gombos <gombosmm@gmail.com>
2022-11-01 17:59:14 -0500

    Rewrite components to classes instead of pure functions

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,28 +1,32 @@
 import React from 'react';
 
-const Calculator = () => (
-  <section className="calculator-container">
-    <p className="result">0</p>
-    <button type="button">AC</button>
-    <button type="button">+/-</button>
-    <button type="button">%</button>
-    <button type="button" className="last-col">÷</button>
-    <button type="button">7</button>
-    <button type="button">8</button>
-    <button type="button">9</button>
-    <button type="button" className="last-col">*</button>
-    <button type="button">4</button>
-    <button type="button">5</button>
-    <button type="button">6</button>
-    <button type="button" className="last-col">-</button>
-    <button type="button">1</button>
-    <button type="button">2</button>
-    <button type="button">3</button>
-    <button type="button" className="last-col">+</button>
-    <button type="button" className="zero">0</button>
-    <button type="button">.</button>
-    <button type="button" className="last-col">=</button>
-  </section>
-);
+class Calculator extends React.Component {
+  render() {
+    return (
+      <section className="calculator-container">
+        <p className="result">0</p>
+        <button type="button">AC</button>
+        <button type="button">+/-</button>
+        <button type="button">%</button>
+        <button type="button" className="last-col">÷</button>
+        <button type="button">7</button>
+        <button type="button">8</button>
+        <button type="button">9</button>
+        <button type="button" className="last-col">*</button>
+        <button type="button">4</button>
+        <button type="button">5</button>
+        <button type="button">6</button>
+        <button type="button" className="last-col">-</button>
+        <button type="button">1</button>
+        <button type="button">2</button>
+        <button type="button">3</button>
+        <button type="button" className="last-col">+</button>
+        <button type="button" className="zero">0</button>
+        <button type="button">.</button>
+        <button type="button" className="last-col">=</button>
+      </section>
+    );
+  }
+}
 
 export default Calculator;
