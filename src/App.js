31e66b76a27359919948fcec7638a222461535cf
Matyas Gombos <gombosmm@gmail.com>
2022-11-01 18:12:30 -0500

    Turn back functions to classes and fix linters

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Calculator from './components/Calculator';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
       <Calculator />
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
       <section className="calculator-container">
