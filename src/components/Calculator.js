aad8bb0e1dc2a95b2f7e2a43d6d45651db3fca37
Matyas Gombos <gombosmm@gmail.com>
2022-11-03 17:04:00 -0500

    Add title and new font

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -18,32 +18,35 @@ const Calculator = () => {
   });
 
   return (
-    <div className="calculator-container">
-      <div className="result">
-        {state.total}
-        {state.operation}
-        {state.next}
-        {state.error}
+    <div className="container">
+      <div className="title">Just MATH it &#10004;</div>
+      <div className="calculator-container">
+        <div className="result">
+          {state.total}
+          {state.operation}
+          {state.next}
+          {state.error}
+        </div>
+        <button type="button" onClick={click}>AC</button>
+        <button type="button" onClick={click}>+/-</button>
+        <button type="button" onClick={click}>%</button>
+        <button type="button" className="last-col" onClick={click}>÷</button>
+        <button type="button" onClick={click}>7</button>
+        <button type="button" onClick={click}>8</button>
+        <button type="button" onClick={click}>9</button>
+        <button type="button" className="last-col" onClick={click}>*</button>
+        <button type="button" onClick={click}>4</button>
+        <button type="button" onClick={click}>5</button>
+        <button type="button" onClick={click}>6</button>
+        <button type="button" className="last-col" onClick={click}>-</button>
+        <button type="button" onClick={click}>1</button>
+        <button type="button" onClick={click}>2</button>
+        <button type="button" onClick={click}>3</button>
+        <button type="button" className="last-col" onClick={click}>+</button>
+        <button type="button" className="zero" onClick={click}>0</button>
+        <button type="button" onClick={click}>.</button>
+        <button type="button" className="last-col" onClick={click}>=</button>
       </div>
-      <button type="button" onClick={click}>AC</button>
-      <button type="button" onClick={click}>+/-</button>
-      <button type="button" onClick={click}>%</button>
-      <button type="button" className="last-col" onClick={click}>÷</button>
-      <button type="button" onClick={click}>7</button>
-      <button type="button" onClick={click}>8</button>
-      <button type="button" onClick={click}>9</button>
-      <button type="button" className="last-col" onClick={click}>*</button>
-      <button type="button" onClick={click}>4</button>
-      <button type="button" onClick={click}>5</button>
-      <button type="button" onClick={click}>6</button>
-      <button type="button" className="last-col" onClick={click}>-</button>
-      <button type="button" onClick={click}>1</button>
-      <button type="button" onClick={click}>2</button>
-      <button type="button" onClick={click}>3</button>
-      <button type="button" className="last-col" onClick={click}>+</button>
-      <button type="button" className="zero" onClick={click}>0</button>
-      <button type="button" onClick={click}>.</button>
-      <button type="button" className="last-col" onClick={click}>=</button>
     </div>
   );
 };
