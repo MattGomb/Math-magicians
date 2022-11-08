8fefd08ed0b179c862b47b6fbf5fdf039cf1a2d3
Matyas Gombos <gombosmm@gmail.com>
2022-11-08 14:45:54 -0500

    Add content of other pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,30 @@
 import React from "react";
+
+function Home() {
+  return (
+    <section className="container">
+      <h2 className="title">Welcome to our page!</h2>
+      <p className="passage">Mathematics is the abstract science of number, quantity, and space. 
+      Mathematics may be studied in its own right (pure mathematics), or as it is applied to other disciplines such as physics and engineering (applied mathematics).
+      Mathematics is essential in the natural sciences, engineering, medicine, finance, computer science and the social sciences. 
+      The fundamental truths of mathematics are independent from any scientific experimentation, although mathematics is extensively used for modeling phenomena. 
+      Some areas of mathematics, such as statistics and game theory, are developed in close correlation with their applications and are often grouped under applied mathematics. 
+      Other mathematical areas are developed independently from any application (and are therefore called pure mathematics), but practical applications are often discovered later.
+      A fitting example is the problem of integer factorization, which goes back to Euclid, but which had no practical application before its use in the RSA cryptosystem 
+      (for the security of computer networks).</p>
+      <p className="passage">Modern electronic calculators vary from cheap, give-away, credit-card-sized models to sturdy desktop models with built-in printers. 
+      They became popular in the mid-1970s as the incorporation of integrated circuits reduced their size and cost. 
+      By the end of that decade, prices had dropped to the point where a basic calculator was affordable to most and they became common in schools.
+      Computer operating systems as far back as early Unix have included interactive calculator programs such as dc and hoc, 
+      and interactive BASIC could be used to do calculations on most 1970s and 1980s home computers. Calculator functions are included in most personal digital assistant (PDA) type devices.
+      In addition to general purpose calculators, there are those designed for specific markets. 
+      For example, there are scientific calculators which include trigonometric and statistical calculations. Some calculators even have the ability to do computer algebra. 
+      Graphing calculators can be used to graph functions defined on the real line, or higher-dimensional Euclidean space. 
+      As of 2016, basic calculators cost little, but scientific and graphing models tend to cost more.
+      With the very wide availability of smartphones, tablet computers and personal computers, dedicated hardware calculators, while still widely used, are less common than they once were. 
+      In 1986, calculators still represented an estimated 41% of the world's general-purpose hardware capacity to compute information. By 2007, this had diminished to less than 0.05%.</p>
+    </section>
+  );
+}
+
+export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,16 @@
 import React from "react";
+
+function Navbar() {
+  return (
+    <header className="navbar-container">
+      <h1 className="navbar-title">Math Magicians</h1>
+      <div className="navbar">
+        <div className="nav-item">Home</div>
+        <div className="nav-item">Calculator</div>
+        <div className="nav-item">Quote</div>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
